bf7d42bd135e7c60cc1717ded608dc5ec256b279
Wonkmy <1589797819@qq.com>
2023-06-27 00:19:44 +0800

    一个简易的登陆功能
    一个简易的登陆功能，目前服务器端还不能保存已经注册的用户数据。也不能生成唯一token

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 var dgram =require("dgram")
 const server = dgram.createSocket('udp4')
 
+class UserInfo{
+    constructor(userId,userName,userPwd){
+        this.userId=userId;
+        this.userName=userName;
+        this.userPwd=userPwd;
+    }
+}
+
+const allUserList=[]
+
 server.on('error',(err)=>{
     console.log(err);
 })
@@ -11,7 +21,35 @@ server.on('listening',()=>{
 })
 
 server.on('message',(msg,rinfo)=>{
-    var requestType=JSON.parse(msg);
+    console.log(rinfo.address+":"+rinfo.port+"="+msg);
+    let loginData = JSON.parse(msg);
+    if(loginData.reqId==0)//登陆请求id
+    {
+        console.log("请求id是0");
+        if(allUserList.length>0){
+            allUserList.forEach(user => {
+                if(loginData.userName==user.userName){//说明列表中有此用户
+                    console.log("有");
+                    if(loginData.userPwd==user.userPwd){//用户名密码正确
+                        //准备发送一个登陆成功的消息
+                        server.send(JSON.stringify({resId:0,resultCode:1}),rinfo.port,rinfo.address)
+                    }
+                }else{
+                    console.log("没有");
+                    let userInfo=new UserInfo(loginData.userId,loginData.userName,loginData.userPwd);
+                    allUserList.push(userInfo)
+                    //创建完之后，发送一个登陆成功的消息
+                    server.send(JSON.stringify({resId:0,resultCode:1}),rinfo.port,rinfo.address)
+                }
+            });
+        }
+        else{
+            let userInfo=new UserInfo(loginData.userId,loginData.userName,loginData.userPwd);
+            allUserList.push(userInfo)
+            //创建完之后，发送一个登陆成功的消息
+            server.send(JSON.stringify({resId:0,resultCode:1}),rinfo.port,rinfo.address)
+        }
+    }
 })
 
 server.bind(9527)
